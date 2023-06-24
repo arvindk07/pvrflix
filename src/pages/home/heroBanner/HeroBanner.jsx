352903cb462c4f8ea1426d777d93f683adde12c6
arvindk07 <info.arvind07@gmail.com>
2023-06-24 14:18:54 +0530

    search routing

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
-  return <div>heero</div>;
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore Now
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for movie or tv show..."
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
