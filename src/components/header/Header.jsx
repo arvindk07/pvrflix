71c5f4586e6f0c4dd22cd73ed8d5788459854ffa
arvindk07 <info.arvind07@gmail.com>
2023-06-24 17:17:41 +0530

    footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
   };
 
   const controlNavbar = () => {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if (window.scrollY > 200) {
       if (window.scrollY > lastScrollY && !mobileMenu) {
         setShow("hidden");
