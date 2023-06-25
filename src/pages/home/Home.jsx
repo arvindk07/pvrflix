8b4c9eaeb592ce97a5e45d516c55576b6bc53d19
arvindk07 <info.arvind07@gmail.com>
2023-06-25 17:25:21 +0530

    top rated and popular components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,18 @@
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
 import "./style.scss";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
+      <Popular />
+      <TopRated />
 
-      <div style={{ height: "1000px" }}></div>
+      {/* <div style={{ height: "1000px" }}></div> */}
     </div>
   );
 };
