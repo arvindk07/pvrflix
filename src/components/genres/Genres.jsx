5da219fcc0c9c4328e6a4b75c5f80601ffb95a16
arvindk07 <info.arvind07@gmail.com>
2023-06-25 11:16:35 +0530

    genres components

---
+++ src/components/genres/Genres.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import "./style.scss";
+
+const Genres = ({ data }) => {
+  const { genres } = useSelector((state) => state.home);
+
+  console.log("gen:", genres);
+  console.log("data:", data);
+
+  return (
+    <div className="genres">
+      {data?.map((g) => {
+        // console.log(JSON.stringify(genres[g]?.name));
+        // if (!genres[g]?.name) return;
+        return (
+          <div className="genre" key={g}>
+            {/* {genres[g]?.name} */}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Genres;
