35185536ad7afbed6b0a6e4376db931172a02f3a
arvindk07 <info.arvind07@gmail.com>
2023-06-29 18:04:24 +0530

    similar and recommended

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,6 +5,8 @@ import VideosSection from "./videoSection/VideoSection";
 
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Similar from "./carousel/Similar";
+import Recommendation from "./carousel/Recommendation";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -17,6 +19,8 @@ const Details = () => {
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoding} />
       <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
