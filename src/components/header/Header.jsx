eef2275f60225c7a108255515e33483a8fc38bf0
arvindk07 <info.arvind07@gmail.com>
2023-06-30 13:04:09 +0530

    search result

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/movix-logo.png";
 
 const Header = () => {
   const [show, setShow] = useState("top");
@@ -73,8 +73,11 @@ const Header = () => {
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
-        <div className="logo">
-          <img src={logo} alt="" />
+        <div className="logo" onClick={() => navigate("/")}>
+          <img src={logo} alt="" />{" "}
+          <h2>
+            PVR<span className="movieLogo">Movie</span>
+          </h2>
         </div>
         <ul className="menuItems">
           <li className="menuItem" onClick={() => navigationHandler("movie")}>
